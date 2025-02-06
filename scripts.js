3f40ed085fcba3ddd39c7babb0113c0e185fd28a
Julio Cesar <julioreis951@gmail.com>
2025-02-06 12:00:44 -0300

    adicionando interação no botão carregar imagem

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const uploadBotao = document.getElementById('upload-btn');
+const inputUpload = document.getElementById('image-upload');
+
+uploadBotao.addEventListener('click', () => {
+    inputUpload.click();
+})
